0d6f38ba161ddb15d407e95dc08f5b512f34bf3a
geovanect <geovanecoutofacs@gmail.com>
2024-10-01 08:52:51 -0300

    adicionado o componente para editar os servidores, mas ainda nao funcional

--- ServerManagement/obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
+++ ServerManagement/obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251a04ebc70bc3fcdafc03126f80274b763009f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
